69d162c6e629ee96ad05df6183dba6ee27ea2ce3
jpapa72 <jpapa@wisc.edu>
2017-03-27 16:36:11 -0500

    Added measureDigital

--- ECE315/interrupts.c
+++ ECE315/interrupts.c
@@ -2,10 +2,12 @@
 #include "adc.h"
 
 volatile bool measureAnalog; 
+volatile bool measureDigital;
 
 int SysTick_Handler(void) {
 	static int count = 0; 
-	int anval;
+	
+	measureDigital = true;
 	
 	if(count == 199) {
 		count = 0;
