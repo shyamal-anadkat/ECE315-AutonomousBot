38eb6e2f1561054292fab9cba30968a10e46c151
jpapa72 <jpapa@wisc.edu>
2017-04-05 14:36:48 -0500

    Added ISRs

--- LAB3/ECE315/interrupts.c
+++ LAB3/ECE315/interrupts.c
@@ -1,2 +1,15 @@
 #include "interrupts.h"
 
+volatile static uint32_t left_pulse = 0;
+volatile static uint32_t right_pulse = 0;
+
+void GPIOF_Handler(void){
+	left_pulse++;
+	GPIOF->ICR |= 1;
+}
+
+
+void GPIOC_Handler(void){
+	right_pulse++;
+	GPIOC->ICR |= 1;
+}
